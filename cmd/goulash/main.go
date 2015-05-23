74afd2bb77da5731f06bc504f9b2775e0fd87d4f
Kris Hicks <krishicks@gmail.com>
2015-05-22 17:22:03 -0700

    Organize variables in main

--- cmd/goulash/main.go
+++ cmd/goulash/main.go
@@ -13,43 +13,47 @@ import (
 )
 
 const (
-	defaultPort = "8080"
+	defaultlistenPort = "8080"
 
-	portVar              = "VCAP_APP_PORT"
+	listenPortVar        = "VCAP_APP_listenPort"
 	tokenVar             = "SLACK_AUTH_TOKEN"
 	teamNameVar          = "SLACK_TEAM_NAME"
 	auditLogChannelIDVar = "SLACK_AUDIT_LOG_CHANNEL_ID"
 )
 
 var (
-	address           string
+	listenPort string
+	listenAddr string
+
 	slackAPI          *slack.Slack
 	slackTeamName     string
 	auditLogChannelID string
-	h                 *handler.Handler
-	logger            lager.Logger
-	timekeeper        clock.Clock
-	port              string
+
+	timekeeper clock.Clock
+	logger     lager.Logger
+	h          *handler.Handler
 )
 
 func init() {
-	if port = os.Getenv(portVar); port == "" {
-		port = defaultPort
+	if listenPort = os.Getenv(listenPortVar); listenPort == "" {
+		listenPort = defaultlistenPort
 	}
+	listenAddr = fmt.Sprintf(":%s", listenPort)
+
 	slackAPI = slack.New(os.Getenv(tokenVar))
 	slackTeamName = os.Getenv(teamNameVar)
 	auditLogChannelID = os.Getenv(auditLogChannelIDVar)
-	address = fmt.Sprintf(":%s", port)
+
+	timekeeper = clock.NewClock()
 	logger = lager.NewLogger("handler")
 	sink := lager.NewReconfigurableSink(lager.NewWriterSink(os.Stdout, lager.DEBUG), lager.DEBUG)
-	timekeeper = clock.NewClock()
 	logger.RegisterSink(sink)
 
 	h = handler.New(slackAPI, slackTeamName, auditLogChannelID, timekeeper, logger)
 }
 
 func main() {
-	if err := http.ListenAndServe(address, h); err != nil {
+	if err := http.ListenAndServe(listenAddr, h); err != nil {
 		log.Fatal("Failed to start server", err)
 	}
 }
