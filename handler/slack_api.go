d04dbf7bc7895a979d7cd5e5eab3a5894b12e547
Kris Hicks <krishicks@gmail.com>
2015-05-27 12:16:18 -0700

    Break SlackAPI methods up with comments

--- handler/slack_api.go
+++ handler/slack_api.go
@@ -7,9 +7,16 @@ import "github.com/pivotalservices/slack"
 // SlackAPI defines the set of methods we expect to call on slack.Slack. This
 // allows us to fake it for testing purposes.
 type SlackAPI interface {
+	// channel
 	PostMessage(channelID string, text string, params slack.PostMessageParameters) (channel string, timestamp string, err error)
+
+	// admin
 	InviteGuest(teamname, channelID, firstName, lastName, emailAddress string) error
 	InviteRestricted(teamname, channelID, firstName, lastName, emailAddress string) error
+
+	// groups
 	GetGroups(excludeArchived bool) ([]slack.Group, error)
+
+	// im
 	OpenIMChannel(userID string) (bool, bool, string, error)
 }
