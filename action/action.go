0e7c7f405ecd7d92b13c7b4adf40f715708d17a7
Kris Hicks <krishicks@gmail.com>
2015-05-29 17:37:00 -0700

    Update help text with 'info' section

--- action/action.go
+++ action/action.go
@@ -149,7 +149,10 @@ func (h help) Do() (string, error) {
 		"_Invite a Single-Channel Guest to the current channel/group_\n" +
 		"\n" +
 		"`invite-restricted <email> <firstname> <lastname>`\n" +
-		"_Invite a Restricted Account to the current channel/group_\n"
+		"_Invite a Restricted Account to the current channel/group_\n" +
+		"\n" +
+		"`info <email>`\n" +
+		"_Get information on a Slack user_\n"
 
 	return text, nil
 }
