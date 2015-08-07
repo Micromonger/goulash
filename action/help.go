c93b799f7769aa6720e4f2fd564eebd46050088d
Kris Hicks <krishicks@gmail.com>
2015-08-07 16:33:18 -0700

    Sort help text by command name

--- action/help.go
+++ action/help.go
@@ -21,23 +21,23 @@ func (h help) Do(
 			"\n"+
 			"*COMMANDS*\n"+
 			"\n"+
+			"`disable-user <email|@username>`\n"+
+			"_Disable a Slack user_\n"+
+			"\n"+
+			"`groups`\n"+
+			"_List the groups that I am in_\n"+
+			"\n"+
+			"`guestify <email|@username>`\n"+
+			"_Convert a Restricted Account to a Single-Channel Guest_\n"+
+			"\n"+
+			"`info <email>`\n"+
+			"_Get information on a Slack user_\n"+
+			"\n"+
 			"`invite-guest <email> <firstname> <lastname>`\n"+
 			"_Invite a Single-Channel Guest to the current channel/group_\n"+
 			"\n"+
 			"`invite-restricted <email> <firstname> <lastname>`\n"+
 			"_Invite a Restricted Account to the current channel/group_\n"+
-			"\n"+
-			"`info <email>`\n"+
-			"_Get information on a Slack user_\n"+
-			"\n"+
-			"`disable-user <email|@username>`\n"+
-			"_Disable a Slack user_\n"+
-			"\n"+
-			"`guestify <email|@username>`\n"+
-			"_Convert a Restricted Account to a Single-Channel Guest_\n"+
-			"\n"+
-			"`groups`\n"+
-			"_List the groups that I am in_\n"+
 			"\n"+
 			"`request-access <#channel>`\n"+
 			"_Request an invitation to a channel_\n",
