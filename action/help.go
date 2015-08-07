9935a2afcfbbe4e6f284c379469e3a16f1ed66ac
Kris Hicks <krishicks@gmail.com>
2015-08-07 16:46:01 -0700

    Use square instead of angle brackets in help
    Maybe this will fix the #deleted-channel problem.

--- action/help.go
+++ action/help.go
@@ -21,25 +21,25 @@ func (h help) Do(
 			"\n"+
 			"*COMMANDS*\n"+
 			"\n"+
-			"`disable-user <email|@username>`\n"+
+			"`disable-user [email|@username]`\n"+
 			"_Disable a Slack user_\n"+
 			"\n"+
 			"`groups`\n"+
 			"_List the groups that @%s is in_\n"+
 			"\n"+
-			"`guestify <email|@username>`\n"+
+			"`guestify [email|@username]`\n"+
 			"_Convert a Restricted Account to a Single-Channel Guest_\n"+
 			"\n"+
-			"`info <email>`\n"+
+			"`info [email]`\n"+
 			"_Get information on a Slack user_\n"+
 			"\n"+
-			"`invite-guest <email> <firstname> <lastname>`\n"+
+			"`invite-guest [email] [firstname] [lastname]`\n"+
 			"_Invite a Single-Channel Guest to the current channel/group_\n"+
 			"\n"+
-			"`invite-restricted <email> <firstname> <lastname>`\n"+
+			"`invite-restricted [email] [firstname] [lastname]`\n"+
 			"_Invite a Restricted Account to the current channel/group_\n"+
 			"\n"+
-			"`request-access <channel>`\n"+
+			"`request-access [#channel]`\n"+
 			"_Request an invitation to a channel_\n",
 		config.SlackSlashCommand(),
 		config.SlackUserID(),
